17f23cc5aa7b166e9726f5270c10d625e5e49716
liuao <1050031287@qq.com>
2023-02-24 15:04:49 +0800

    pytorch 环境，使用比较新的格式对AE网络进行书写

---
+++ my_ae/ae.py
@@ -0,0 +1,30 @@
+import torch
+import torch.nn as nn
+
+class AutoEncoder(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
+        # [b, 784]
+        self.encoder = nn.Sequential(
+            nn.Linear(784,256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256,64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64,20),
+            nn.ReLU(inplace=True)
+        )
+        self.decoder = nn.Sequential(
+            nn.Linear(20,64),
+            nn.ReLU(inplace=True),
+            nn.Linear(64,256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256,784),
+            nn.Sigmoid(),
+        )
+
+    def forward(self, x):
+        x = x.flatten(1)
+        x = self.encoder(x)
+        x = self.decoder(x)
+        x = x.view(-1,1,28,28)
+        return x
---
+++ my_ae/main.py
@@ -0,0 +1,48 @@
+from random import shuffle
+import torch
+import torch.nn as nn
+from torch.utils.data import DataLoader
+from torchvision import datasets, transforms
+from ae import AutoEncoder
+import visdom
+
+
+def main():
+    mnist_train = datasets.MNIST('data/', train=True, download=True, transform=transforms.Compose([
+        transforms.ToTensor()]
+    ))
+    mnist_test = datasets.MNIST('data/', train=False, download=True, transform=transforms.Compose([
+        transforms.ToTensor()]
+    ))
+
+    train_loader = DataLoader(mnist_train, batch_size=32, shuffle=True)
+    test_loader = DataLoader(mnist_test, batch_size=32, shuffle=False)
+
+    x, y = iter(train_loader).next()
+    print(x.shape)
+
+    device = torch.device('cpu')
+    model = AutoEncoder().to(device)
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=1e-3)
+
+    vis = visdom.Visdom()
+    for epoch in range(1000):
+        for batch_idx, (x, y) in enumerate(train_loader):
+            x, y = x.to(device), y.to(device)
+            x_hat = model(x)
+            loss = criterion(x_hat, x)
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(epoch, 'loss:', loss.item())
+        x, y = iter(test_loader).next()
+        with torch.no_grad():
+            x_hat = model(x)
+        vis.images(x, nrow=8, win='x-ae', opts=dict(title='x'))
+        vis.images(x_hat, nrow=8, win='x-hat', opts=dict(title='x_hat'))
+
+
+if __name__ == '__main__':
+    main()
